cdf2a04201157e899fbe28bcd8769387ba316cdc
May Pongpitpitak <me@mayppong.com>
2015-01-17 23:25:11 -0800

    Add set up script for OS X

--- setup-osx.sh
+++ setup-osx.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/sh
 
+# copy necessary files
+make install
+
+# perform generic set up
+sh ./setup.sh
+
 # install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew tap caskroom/versions
+brew cask install google-chrome google-drive google-hangouts keepassx0 vagrant virtualbox colloquy sublime-text3
