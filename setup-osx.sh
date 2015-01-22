543a15244fce466a3deab02b087ef47ca497905b
May Pongpitpitak <me@mayppong.com>
2015-01-21 17:48:11 -0800

    Gotta install homebrew cask before using it!

--- setup-osx.sh
+++ setup-osx.sh
@@ -9,5 +9,6 @@ sh ./setup.sh
 # install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
+brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 brew cask install google-chrome google-drive google-hangouts keepassx0 vagrant virtualbox colloquy sublime-text3
