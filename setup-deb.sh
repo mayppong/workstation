524d68af6c6ecc1e2d87f00375ff42e0365979e3
May Pongpitpitak <me@mayppong.com>
2015-02-17 08:44:56 +0700

    Install zsh before making it default

--- setup-deb.sh
+++ setup-deb.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+# install apps
+apt-get install trash-cli zsh
+
 # copy necessary files
 make install-deb
 
 # perform generic set up
 sh ./setup.sh
-
-# install apps
-apt-get install trash-cli zsh
