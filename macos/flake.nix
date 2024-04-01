dc08ab922f9b97ff13b8371fc900f24fe83a6325
May Pongpitpitak <>
2024-04-01 12:53:09 +0700

    Update darwin flake
    We're switching to using `CustomUserPreferences` instead for some of the
    behaviour as some are not available directly through nix-darwin.
    
    Also add `.gitignore` to not check in result of running nix

--- macos/flake.nix
+++ macos/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "ZynthiaPro Big Sur setup";
+  description = "ZynthiaAir Sonoma setup";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -10,13 +10,17 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
       # List packages installed in system profile. To search by name, run:
       # `$ nix-env -qaP | grep wget`
       # environment.systemPackages = [];
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+      nix.package = pkgs.nix;
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
@@ -30,24 +34,39 @@
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
       # Mac OS behaviours
       # https://daiderd.com/nix-darwin/manual/index.html
       system.defaults = {
-        dock.autohide = true;
-        # hot corner action: mission control
-        dock.wvous-tl-corner = 2;
-        dock.wvous-tr-corner = 2;
+        dock = {
+          autohide = true;
+
+          # hot corner action: mission control
+          wvous-tl-corner = 2;
+          wvous-tr-corner = 2;
+        };
 
         screencapture.location = "~/Pictures/Screenshots";
-        screensaver.askForPasswordDelay = 10;
 
         # allow trackpad tap to click
-        trackpad.Clicking = true;
-        trackpad.TrackpadRightClick = true;
+        NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
+        trackpad = {
+          Clicking = true;
+          TrackpadRightClick = true;
+        };
+
+        # Custom config not available directly through nix-darwin can be set
+        # through this `CustomUserPreferences`.
+        # https://medium.com/@zmre/nix-darwin-quick-tip-activate-your-preferences-f69942a93236
+        # https://macos-defaults.com/
+        CustomUserPreferences = {
+          "com.apple.desktopservices" = {
+            # Avoid creating .DS_Store files on network or USB volumes
+            DSDontWriteNetworkStores = true;
+            DSDontWriteUSBStores = true;
+          };
+
+          "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+        };
       };
 
       # The platform the configuration will be used on.
